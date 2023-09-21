2d478f267eae33e2c59194ce0833c0c17cc69c9c
Baauerr <artem_bauer@inbox.ru>
2023-09-21 12:49:39 +0700

    Initial commit

---
+++ src/main/java/com/example/shiftjija/ShiftJijaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftjija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftJijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftJijaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shiftjija/ShiftJijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiftjija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftJijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
